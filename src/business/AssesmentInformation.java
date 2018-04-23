c0687d39f343581413670d48f0ddf40c21f63e7b
michaeltrier1 <32192126+michaeltrier1@users.noreply.github.com>
2018-04-23 14:24:57 +0200

    AssessmentInformation

--- src/business/AssesmentInformation.java
+++ src/business/AssesmentInformation.java
@@ -5,10 +5,34 @@
  */
 package business;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  *
  * @author malte
  */
 public class AssesmentInformation {
-    
-}
+  private HashMap<String, String> textInformation;
+  private List<File> files;
+  
+  public AssesmentInformation(){
+    textInformation =   new HashMap<>();
+    files = new ArrayList<>();
+  }
+  public Set<String> validateContent(){
+   return null;   
+  }
+  public void write(String text, String sourceInfo){
+   textInformation.put(sourceInfo, text);
+   
+   
+         
+      
+  } 
+  }
+
