7cc47135775b25aabca285d9653f08f5969906c6
michaeltrier1 <32192126+michaeltrier1@users.noreply.github.com>
2018-04-24 15:22:45 +0200

    Information and caseInformation DONE

---
+++ src/business/AssessmentInformation.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @author malte
+ */
+public class AssessmentInformation {
+  private HashMap<String, String> textInformation;
+
+  
+  public AssessmentInformation(){
+    textInformation =   new HashMap<>();
+  }
+  public Set<String> getFilledFields(){
+      
+   return textInformation.keySet();   
+  }
+  public void write(String text, String sourceInfo){
+      textInformation.put(sourceInfo, text);
+      if (text == null||text == ""){
+        textInformation.remove(sourceInfo);
+        
+      }
+   
+   
+         
+      
+  } 
+  }
+
--- src/business/CaseInformation.java
+++ src/business/CaseInformation.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -5,10 +6,25 @@
  */
 package business;
 
+import java.util.HashMap;
+
 /**
  *
- * @author malte
+ * @author michael
  */
 public class CaseInformation {
+ private HashMap<String, String> textInformation;
+ 
+ public CaseInformation(){
+     
+ }
+ public CaseInformation(HashMap<String, String> textInformation){
+     this.textInformation = textInformation;
+ }
+ 
+ HashMap<String, String> getInformation(){
+    return textInformation;
+     
+ }
     
 }
