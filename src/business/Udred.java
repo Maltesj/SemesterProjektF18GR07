f6a2636f99fcf2d0fb6d38265690bb6cd6b40055
simonnj <32192443+simonnj@users.noreply.github.com>
2018-04-24 17:22:51 +0200

    udred

--- src/business/Udred.java
+++ src/business/Udred.java
@@ -7,6 +7,7 @@ package business;
 
 import acquaintance.IDataFacade;
 import java.util.Map;
+import java.util.Set;
 
 /**
  *
@@ -16,6 +17,7 @@ public class Udred {
     private String currentCaseWorkerID;
     private String currentCaseID;
     private Map<String, Information> cases;
+    private CheckList checkList;
     
     public boolean startAssessment(String caseID , String caseWorkerID) {
         this.currentCaseID = caseID;
@@ -46,5 +48,28 @@ public class Udred {
         
     }
     
+    public Set<String> done(){
+        Information info = cases.get(currentCaseID);
+        Set<String> filedAssessment = info.getFilledAssessmentFields();
+        
+        Set<String> missingFields = checkList.checkCollection(filedAssessment);
+        
+        BusinessFacade business = BusinessFacade.getInstance();
+        IDataFacade data = business.getDataFacade();
+        data.save(info);
+        
+        return missingFields;
+    }
+    public void write(String text, String sourceInfo){
+        Information info = cases.get(text);
+        info.write(text, sourceInfo);
+        
+    }
+    public Map<String, String> getCaseInformation(){
+        Information info = cases.get(currentCaseID);
+        Map<String, String> caseinfo = info.getCaseInformation();
+        
+        return  caseinfo;
+    }
     
 }
