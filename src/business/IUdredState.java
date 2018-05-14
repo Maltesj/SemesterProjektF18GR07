603cef313a7aeb577a3e1b819d80d196327202fe
michaeltrier1 <32192126+michaeltrier1@users.noreply.github.com>
2018-05-14 14:59:33 +0200

    virker

--- src/business/IUdredState.java
+++ src/business/IUdredState.java
@@ -14,18 +14,11 @@ import java.util.Set;
  */
 public interface IUdredState {
     
-<<<<<<< HEAD
+
     boolean savePhase(Information information);
     void write(String text, String sourceInfo, Information information);
     Set<String> checkFields(Information information);
     Set<String> done(Information information);
     void discard(Information information);
-=======
-    boolean savePhase(Information infoHolder);
-    void write(String text, String sourceInfo);
-    Set<String> checkFields();
-    Set<String> done();
-    void discard(Information infoHolder);
->>>>>>> 9506661273824e0bde851cab2345a72f55ad9181
-    
+
 }
