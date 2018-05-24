2ecc18d4cf08f1e6b4261998f2e20cee07e496d0
HCHB <32193248+HCHB@users.noreply.github.com>
2018-05-24 17:12:53 +0200

    Merge origin/master
    Conflicts:
    	src/generator/SQLDatabaseResetter.java

--- src/data/LoadDatabase.java
+++ src/data/LoadDatabase.java
@@ -62,36 +62,24 @@ public class LoadDatabase {
         
         IWork work = null;
         try (Connection db = DriverManager.getConnection(EnumDatabaseAccount.ACCOUNT1.url, EnumDatabaseAccount.ACCOUNT1.userName, EnumDatabaseAccount.ACCOUNT1.password)) {
-//            PreparedStatement st = db.prepareStatement("Select ? "
-//                                                     + "From cases "
-//                                                     + "Where caseID = '?' "
-//                                                     + "AND date_last_changed = '(Select MAX(date_last_Changed) "
-//                                                                             + "From cases "
-//                                                                             + "Where caseID = '?')'");
-
-//            st.setString(0, "work");
-//            st.setString(1, "work");
-//            st.setString(2, caseID);
-//            st.setString(3, caseID);
-//            ResultSet results =  st.executeQuery();
-
-Statement statement = db.createStatement();
-ResultSet results = statement.executeQuery("Select work "
-        + "From cases "
-        + "Where caseID = '"+caseID+"' "
-                + "AND date_last_changed = (Select MAX(date_last_Changed) "
-                + "From cases "
-                + "Where caseID = '"+caseID+"')");
-
-results.next();
-byte[] bytes = results.getBytes(1);
-
-ByteArrayInputStream byteArrayIS = new ByteArrayInputStream(bytes);
-ObjectInputStream objectIS = new ObjectInputStream(byteArrayIS);
-
-work = (IWork)objectIS.readObject();
-
-return work;
+            
+            Statement statement = db.createStatement();
+            ResultSet results = statement.executeQuery("Select work "
+                    + "From cases "
+                    + "Where caseID = '"+caseID+"' "
+                            + "AND date_last_changed = (Select MAX(date_last_Changed) "
+                            + "From cases "
+                            + "Where caseID = '"+caseID+"')");
+            
+            results.next();
+            byte[] bytes = results.getBytes(1);
+            
+            ByteArrayInputStream byteArrayIS = new ByteArrayInputStream(bytes);
+            ObjectInputStream objectIS = new ObjectInputStream(byteArrayIS);
+            
+            work = (IWork)objectIS.readObject();
+            
+            return work;
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -108,36 +96,24 @@ return work;
         
         IAssessment assessment = null;
         try (Connection db = DriverManager.getConnection(EnumDatabaseAccount.ACCOUNT1.url, EnumDatabaseAccount.ACCOUNT1.userName, EnumDatabaseAccount.ACCOUNT1.password)) {
-//            PreparedStatement st = db.prepareStatement("Select ? "
-//                                                     + "From cases "
-//                                                     + "Where caseID = '?' "
-//                                                     + "AND date_last_changed = '(Select MAX(date_last_Changed) "
-//                                                                             + "From cases "
-//                                                                             + "Where caseID = '?')'");
-
-//            st.setString(0, "work");
-//            st.setString(1, "work");
-//            st.setString(2, caseID);
-//            st.setString(3, caseID);
-//            ResultSet results =  st.executeQuery();
-
-Statement statement = db.createStatement();
-ResultSet results = statement.executeQuery("Select assessment "
-        + "From cases "
-        + "Where caseID = '"+caseID+"' "
-                + "AND date_last_changed = (Select MAX(date_last_Changed) "
-                + "From cases "
-                + "Where caseID = '"+caseID+"')");
-
-results.next();
-byte[] bytes = results.getBytes(1);
-
-ByteArrayInputStream byteArrayIS = new ByteArrayInputStream(bytes);
-ObjectInputStream objectIS = new ObjectInputStream(byteArrayIS);
-
-assessment = (IAssessment)objectIS.readObject();
-
-return assessment;
+            
+            Statement statement = db.createStatement();
+            ResultSet results = statement.executeQuery("Select assessment "
+                    + "From cases "
+                    + "Where caseID = '"+caseID+"' "
+                            + "AND date_last_changed = (Select MAX(date_last_Changed) "
+                            + "From cases "
+                            + "Where caseID = '"+caseID+"')");
+            
+            results.next();
+            byte[] bytes = results.getBytes(1);
+            
+            ByteArrayInputStream byteArrayIS = new ByteArrayInputStream(bytes);
+            ObjectInputStream objectIS = new ObjectInputStream(byteArrayIS);
+            
+            assessment = (IAssessment)objectIS.readObject();
+            
+            return assessment;
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -154,36 +130,24 @@ return assessment;
         
         IActionplan actionplan = null;
         try (Connection db = DriverManager.getConnection(EnumDatabaseAccount.ACCOUNT1.url, EnumDatabaseAccount.ACCOUNT1.userName, EnumDatabaseAccount.ACCOUNT1.password)) {
-//            PreparedStatement st = db.prepareStatement("Select ? "
-//                                                     + "From cases "
-//                                                     + "Where caseID = '?' "
-//                                                     + "AND date_last_changed = '(Select MAX(date_last_Changed) "
-//                                                                             + "From cases "
-//                                                                             + "Where caseID = '?')'");
-
-//            st.setString(0, "work");
-//            st.setString(1, "work");
-//            st.setString(2, caseID);
-//            st.setString(3, caseID);
-//            ResultSet results =  st.executeQuery();
-
-Statement statement = db.createStatement();
-ResultSet results = statement.executeQuery("Select actionplan "
-        + "From cases "
-        + "Where caseID = '"+caseID+"' "
-                + "AND date_last_changed = (Select MAX(date_last_Changed) "
-                + "From cases "
-                + "Where caseID = '"+caseID+"')");
-
-results.next();
-byte[] bytes = results.getBytes(1);
-
-ByteArrayInputStream byteArrayIS = new ByteArrayInputStream(bytes);
-ObjectInputStream objectIS = new ObjectInputStream(byteArrayIS);
-
-actionplan = (IActionplan)objectIS.readObject();
-
-return actionplan;
+            
+            Statement statement = db.createStatement();
+            ResultSet results = statement.executeQuery("Select actionplan "
+                    + "From cases "
+                    + "Where caseID = '"+caseID+"' "
+                            + "AND date_last_changed = (Select MAX(date_last_Changed) "
+                            + "From cases "
+                            + "Where caseID = '"+caseID+"')");
+            
+            results.next();
+            byte[] bytes = results.getBytes(1);
+            
+            ByteArrayInputStream byteArrayIS = new ByteArrayInputStream(bytes);
+            ObjectInputStream objectIS = new ObjectInputStream(byteArrayIS);
+            
+            actionplan = (IActionplan)objectIS.readObject();
+            
+            return actionplan;
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -200,6 +164,81 @@ return actionplan;
         
         ICaseInformation caseInfo = null;
         try (Connection db = DriverManager.getConnection(EnumDatabaseAccount.ACCOUNT1.url, EnumDatabaseAccount.ACCOUNT1.userName, EnumDatabaseAccount.ACCOUNT1.password)) {
+            
+            Statement statement = db.createStatement();
+            ResultSet results = statement.executeQuery("Select case_information "
+                    + "From cases "
+                    + "Where caseID = '"+caseID+"' "
+                            + "AND date_last_changed = (Select MAX(date_last_Changed) "
+                            + "From cases "
+                            + "Where caseID = '"+caseID+"')");
+            
+            results.next();
+            byte[] bytes = results.getBytes(1);
+            
+            ByteArrayInputStream byteArrayIS = new ByteArrayInputStream(bytes);
+            ObjectInputStream objectIS = new ObjectInputStream(byteArrayIS);
+            
+            caseInfo = (ICaseInformation)objectIS.readObject();
+            
+            return caseInfo;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
+        return caseInfo; // temp
+    }
+    
+    public static void main(String[] args) {
+        String caseID = "DummyCase1";
+        LoadDatabase load = new LoadDatabase();
+        
+//        IWork work = load.loadWork(caseID);
+//        ICaseInformation info = load.loadCaseInformation(caseID);
+//        IAssessment ass = load.loadAssessment(caseID);
+//        IActionplan action = load.loadActionplan(caseID);
+        load.updateTest();
+        
+        System.out.println("");
+    }
+    
+    private void updateTest(){
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (java.lang.ClassNotFoundException e) {
+            System.out.println(e);
+        }
+        
+        try (Connection db = DriverManager.getConnection(EnumDatabaseAccount.ACCOUNT1.url, EnumDatabaseAccount.ACCOUNT1.userName, EnumDatabaseAccount.ACCOUNT1.password)) {
+            
+            Statement statement = db.createStatement();
+            statement.executeUpdate("Insert into cases("
+                    + "date_last_changed, "
+                    + "caseID, "
+                    + "date_created, "
+                    + "work, "
+                    + "assessment, "
+                    + "actionplan, "
+                    + "case_information "
+                    + ") values(CAST('2006-05-09' AS DATE), "
+                    + "(Select caseID From cases where caseID = 'DummyCase1' AND date_last_changed = (select max(date_last_changed) From cases where caseID = 'DummyCase1')), "
+                    + "(Select date_created From cases where caseID = 'DummyCase1' AND date_last_changed = (select max(date_last_changed) From cases where caseID = 'DummyCase1')), "
+                    + "(Select work From cases where caseID = 'DummyCase1' AND date_last_changed = (select max(date_last_changed) From cases where caseID = 'DummyCase1')), "
+                    + "(Select assessment From cases where caseID = 'DummyCase1' AND date_last_changed = (select max(date_last_changed) From cases where caseID = 'DummyCase1')), "
+                    + "(Select actionplan From cases where caseID = 'DummyCase1' AND date_last_changed = (select max(date_last_changed) From cases where caseID = 'DummyCase1')), "
+                    + "(Select case_information From cases where caseID = 'DummyCase1' AND date_last_changed = (select max(date_last_changed) From cases where caseID = 'DummyCase1')))");
+                                     
+            
+//            , date_created, work, assessment, actionplan, case_information
+//            
+//            + "ORDER BY caseID "
+//                                     + "DESC LIMIT 1))");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
 //            PreparedStatement st = db.prepareStatement("Select ? "
 //                                                     + "From cases "
 //                                                     + "Where caseID = '?' "
@@ -212,40 +251,3 @@ return actionplan;
 //            st.setString(2, caseID);
 //            st.setString(3, caseID);
 //            ResultSet results =  st.executeQuery();
-
-Statement statement = db.createStatement();
-ResultSet results = statement.executeQuery("Select case_information "
-        + "From cases "
-        + "Where caseID = '"+caseID+"' "
-                + "AND date_last_changed = (Select MAX(date_last_Changed) "
-                + "From cases "
-                + "Where caseID = '"+caseID+"')");
-
-results.next();
-byte[] bytes = results.getBytes(1);
-
-ByteArrayInputStream byteArrayIS = new ByteArrayInputStream(bytes);
-ObjectInputStream objectIS = new ObjectInputStream(byteArrayIS);
-
-caseInfo = (ICaseInformation)objectIS.readObject();
-
-return caseInfo;
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        
-        return caseInfo; // temp
-    }
-    
-    public static void main(String[] args) {
-        String caseID = "DummyCase1";
-        LoadDatabase load = new LoadDatabase();
-        
-        IWork work = load.loadWork(caseID);
-        ICaseInformation info = load.loadCaseInformation(caseID);
-        IAssessment ass = load.loadAssessment(caseID);
-        IActionplan action = load.loadActionplan(caseID);
-        
-        System.out.println("");
-    }
-}
